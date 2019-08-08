e4d44f85020f313105c7308a70792d0306de4f0d
Stuart Morris <admin@handcode.co.uk>
2019-08-08 12:15:48 +0100

    Load flash page for 3 seconds on initial app load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import GlobalStyles from "./global-styles";
+import Login from "./pages/Login";
+import Flash from "./pages/Flash";
 
 const App = () => {
+    const [showFlash, setShowFlash] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setShowFlash(false);
+        }, 3000);
+
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
+
     return (
         <div>
-            <GlobalStyles />
-            <h1>App</h1>
+            <Router>
+                <GlobalStyles />
+                <Route path='/' render={() => (showFlash ? <Flash /> : <Login />)} />
+            </Router>
         </div>
     );
 };

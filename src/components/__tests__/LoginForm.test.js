019f0703a46dce72f00419613c4ba3144613a00a
Stuart Morris <admin@handcode.co.uk>
2019-08-09 09:46:31 +0100

    Test: should not be allowed to click the log in button on load

--- src/components/__tests__/LoginForm.test.js
+++ src/components/__tests__/LoginForm.test.js
@@ -2,7 +2,8 @@ import React from "react";
 import { mount } from "enzyme";
 import LoginForm from "../LoginForm/LoginForm";
 import { AuthProvider } from "../../context/AuthContext";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Button from "../Button/Button";
 
 let wrapper;
 
@@ -19,5 +20,10 @@ describe("<LoginForm />", () => {
 
     test("should render", () => {
         expect(wrapper).toBeTruthy();
+    });
+
+    test("should not be allowed to click the log in button on load", () => {
+        const button = wrapper.find(".form-group.submit").find(Button);
+        expect(button.props().disabled).toBeTruthy();
     });
 });
